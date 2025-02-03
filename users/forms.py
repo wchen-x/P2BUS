8e3b5289a59f90118c2967dbe2f3d26655261e42
wchen-x <cw250105@gmail.com>
2025-02-03 15:15:48 +0000

    Merge forms

--- users/forms.py
+++ users/forms.py
@@ -21,3 +21,37 @@ class UserRegistrationForm(UserCreationForm):
                 user=user,
             )
         return user
+        
+class UserLoginForm(forms.Form):
+    """Login form for authenticating users."""
+    username = forms.CharField(max_length=150, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is None:
+                raise forms.ValidationError("Invalid username or password")
+        return cleaned_data
+    
+class UserProfileForm(forms.ModelForm):
+    """Form for updating user profile information"""
+    phone_number = forms.CharField(max_length=15, required=False, label="Phone Number")
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+class UserAddressForm(forms.ModelForm):
+    """Form for adding/editing addresses"""
+    class Meta:
+        model = Address
+        fields = [
+            "address_line_1", "address_line_2", "city", "state", 
+            "postal_code", "country", "is_primary",
+        ]
+
